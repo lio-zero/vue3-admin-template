fe1b9ad4b051bef88a013a9600adaaa7e16f2790
lio-zero <licroning@163.com>
2022-05-20 18:19:47 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import 'virtual:svg-icons-register'
-
-import 'normalize.css/normalize.css'
-import 'nprogress/nprogress.css'
-import '@/styles/index.scss'
-
 import App from './App.vue'
 import { createApp } from 'vue'
 import { setupStore } from '@/store'
 import { setupRouter } from '@/router'
+import 'normalize.css/normalize.css'
+import 'nprogress/nprogress.css'
+import '@/styles/index.scss'
 import { initAppConfigStore } from '@/logics/initAppConfig'
 import { setupElIcon } from '@/components/Icon'
 
